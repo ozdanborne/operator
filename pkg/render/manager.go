896e1f898b60017ef5c443c2a30f539ef646ebe8
ozdanborne <dan@projectcalico.org>
2022-07-15 10:16:31 -0400

    fixes

--- pkg/render/manager.go
+++ pkg/render/manager.go
@@ -92,6 +92,13 @@ func Manager(cfg *ManagerConfiguration) (Component, error) {
 	if cfg.ManagementCluster != nil {
 		tlsAnnotations[cfg.InternalTrafficSecret.HashAnnotationKey()] = cfg.InternalTrafficSecret.HashAnnotationValue()
 		tlsAnnotations[cfg.TunnelSecret.HashAnnotationKey()] = cfg.InternalTrafficSecret.HashAnnotationValue()
+
+		if cfg.TunnelServerSecret != nil {
+			tlsAnnotations[cfg.TunnelServerSecret.HashAnnotationKey()] = cfg.TunnelServerSecret.HashAnnotationValue()
+
+			// TODO: does this work / is this necessary?
+			tlsSecrets = append(tlsSecrets, cfg.TunnelSecret.Secret(ManagerNamespace))
+		}
 	}
 	return &managerComponent{
 		cfg:            cfg,
@@ -190,6 +197,10 @@ func (c *managerComponent) Objects() ([]client.Object, []client.Object) {
 	objs = append(objs, c.managerDeployment())
 	if c.cfg.KeyValidatorConfig != nil {
 		objs = append(objs, configmap.ToRuntimeObjects(c.cfg.KeyValidatorConfig.RequiredConfigMaps(ManagerNamespace)...)...)
+	}
+
+	if c.cfg.TunnelServerSecret != nil {
+		objs = append(objs, c.cfg.TunnelServerSecret.Secret(ManagerNamespace))
 	}
 
 	return objs, nil
@@ -291,6 +302,9 @@ func (c *managerComponent) managerVolumes() []corev1.Volume {
 	}
 	if c.cfg.KeyValidatorConfig != nil {
 		v = append(v, c.cfg.KeyValidatorConfig.RequiredVolumes()...)
+	}
+	if c.cfg.TunnelServerSecret != nil {
+		v = append(v, c.cfg.TunnelServerSecret.Volume())
 	}
 
 	return v
@@ -462,6 +476,10 @@ func (c *managerComponent) volumeMountsForProxyManager() []corev1.VolumeMount {
 	if c.cfg.ManagementCluster != nil {
 		mounts = append(mounts, c.cfg.InternalTrafficSecret.VolumeMount())
 		mounts = append(mounts, c.cfg.TunnelSecret.VolumeMount())
+	}
+
+	if c.cfg.TunnelServerSecret != nil {
+		mounts = append(mounts, c.cfg.TunnelServerSecret.VolumeMount())
 	}
 
 	return mounts
